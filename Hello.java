27cd01368128317b414430a940c949c6529a07be
Shiv Shikhar Sinha <shiv.shikhar.7@gmail.com>
2022-10-14 22:32:47 +0530

    added a hello-world program in  JAVA language.

---
+++ Hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hello{
+    public static void main(String[] args) {
+ 
+//  This is my try to contribute to a github repository under the hacktoberfest.
+ 
+ System.out.println("Enter number of times you want to print Hello World: ");
+ Scanner sc = new Scanner (System.in);
+ int n = sc.nextInt();
+        while (n-->0){
+            
+        System.out.println("Hello World");
+        }
+ sc.close();
+    }
+}
